f1918cce2f8d87bec93ee93dbad8f645af4ff60f
Miguel Ángel Bernal Colonia - Ceiba Software <miguel.bernal@ceiba.com.co>
2021-07-20 17:12:37 -0500

    fix assert in reserva list test

--- microservicio/infraestructura/src/test/java/com/ceiba/reserva/controlador/ConsultaControladorReservaTest.java
+++ microservicio/infraestructura/src/test/java/com/ceiba/reserva/controlador/ConsultaControladorReservaTest.java
@@ -37,7 +37,7 @@ public class ConsultaControladorReservaTest {
                 .param("fecha", fecha.toString())
                 .contentType(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("$", hasSize(1)))
+                .andExpect(jsonPath("$", hasSize(2)))
                 .andExpect(jsonPath("$[0].id", is(1)));
     }
 
