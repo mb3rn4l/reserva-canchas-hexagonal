d371c6ad6b2d58ffd5f77f8bb993c3e2473449d7
Miguel Ángel Bernal Colonia - Ceiba Software <miguel.bernal@ceiba.com.co>
2021-07-22 08:33:27 -0500

    se borra repositorio sin uso

--- microservicio/dominio/src/main/java/com/ceiba/tipocancha/puerto/repositorio/RepositorioTipoCancha.java
+++
@@ -1,9 +0,0 @@
-/*package com.ceiba.tipocancha.puerto.repositorio;
-
-import com.ceiba.tipocancha.modelo.entidad.TipoCancha;
-
-public interface RepositorioTipoCancha {
-
-    TipoCancha obtenerTipoCanchaPorId(Long idTipoCancha);
-
-}*/
